afbb780941760893bb58fb75af16e645ab61dc24
Brian Hague <bhague1281@gmail.com>
2015-08-14 17:39:14 -0700

    Post time via moment

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,16 @@
 $(function() {
   var socket = io();
   $('form').submit(function() {
-    socket.emit('chat message', $('#m').val());
+    if ($('#m').val())
+      socket.emit('chat message', $('#m').val());
     $('#m').val('');
     return false;
   });
   socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text(msg));
+    $('#messages').prepend($('<li class="collection-item">').text(moment().format("h:mmA") + " - " + msg));
   });
   socket.on('chat alert', function(msg) {
-    var boldedAlert = $('<strong>').text(msg);
-    $('#messages').append($('<li>').append(boldedAlert));
+    var boldedAlert = $('<strong>').text(moment().format("h:mmA") + " - " + msg);
+    $('#messages').prepend($('<li class="collection-item deep-orange lighten-4">').append(boldedAlert));
   });
 });
